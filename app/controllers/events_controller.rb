be127d7cabe1820525eb2f78afcd07d0bb43a43f
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-12-13 15:14:02 +1100

    Initial setup of Event model

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,18 @@
 class EventsController < ApplicationController
+
     def all_events
-        Event.all
+        events = Event.all
+
+        render json: events
+    end
+
+    def show_event
+        event = Event.find_by(id: params[:event_id])
+
+        render json: event
+
+        if !event
+            render "no_event"
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # GET /events
   get "/events", to: "events#all_events"
   # events_controller.rb
+
+  # GET events by id
+  get "/events/:event_id", to: "events#show_event"
+
+  # GET events index
+  get "/events", to: "events#index"
 end
