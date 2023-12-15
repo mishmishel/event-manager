8b77e576f26c1917f15990849d5aac5657bfa045
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-12-15 13:32:09 +1100

    Working routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ Rails.application.routes.draw do
   get "/events", to: "events#all_events"
   # events_controller.rb
 
-  # GET events by id
-  get "/events/:event_id", to: "events#show_event"
-
-  # GET all events that have battle = true
-  get "/battles", to: "events#all_battles"
-
   # GET all events that have jam = true
   get "/events/jams", to: "events#all_jams"
+
+   # GET all events that have battle = true
+   get "/events/battles", to: "events#all_battles"
+
+  # GET events by id
+  get "/events/:event_id", to: "events#show_event"
 
 end
 
