48a6804088d5ed2f017200e5d2d8a048800b709a
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-12-18 13:45:34 +1100

    Index, Show, Create restful routes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,8 @@
 class EventsController < ApplicationController
 
-    def all_events
+    skip_before_action :verify_authenticity_token
+
+    def index
         events = Event.all
 
         if events.empty?
@@ -10,19 +12,46 @@ class EventsController < ApplicationController
         end
     end
 
-    def show_event
-        event = Event.find_by(id: params[:event_id])
+    def show
+        event = Event.find_by(id: params[:id])
 
+        if !event
+            render json: {status: "No events yet"}
+        else
+            render json: event, except: [:created_at, :updated_at, :id]
+        end
+    end
+
+    def create
+        title = params["title"]
+        description = params["description"]
+        date = params["date"]
+        battle = params["battle"]
+        jam = params["jam"]
+
+        #parse date string into a Date object
+        parsed_date = Date.parse(date)
+
+        event = Event.create(
+            title: title,
+            description: description,
+            date: parsed_date,
+            battle: battle, 
+            jam: jam
+        )
+        
         render json: event, except: [:created_at, :updated_at, :id]
     end
 
-    def all_battles
-        battles = Event.where(battle: true)
-        render json: battles, except: [:created_at, :updated_at, :id]
-    end
+    # def all_battles
+    #     battles = Event.where(battle: true)
+    #     render json: battles, except: [:created_at, :updated_at, :id]
+    # end
 
-    def all_jams
-        jams = Event.where(jam: true)
-        render json: jams, except: [:created_at, :updated_at, :id]
-    end
+    # def all_jams
+    #     jams = Event.where(jam: true)
+    #     render json: jams, except: [:created_at, :updated_at, :id]
+    # end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
 Rails.application.routes.draw do
+
+  resources :events
+
   # GET /events
-  get "/events", to: "events#all_events"
+  get "/events", to: "events#index"
   # events_controller.rb
 
-  # GET all events that have jam = true
-  get "/events/jams", to: "events#all_jams"
+  # POST to create new events
+  post "/events", to: "events#create"
 
-   # GET all events that have battle = true
-   get "/events/battles", to: "events#all_battles"
+  # GET events by id - ensure ID route is under /battles and /jams to avoid issues
+  get "/events/:id", to: "events#show"
 
-  # GET events by id
-  get "/events/:event_id", to: "events#show_event"
+  # # GET all events that have jam = true
+  # get "/events/:jams", to: "events#all_jams"
+
+  # # GET all events that have battle = true
+  # get "/events/:battles", to: "events#all_battles"
 
 end
 
