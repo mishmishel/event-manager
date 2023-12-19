f66219bd5a3730a7ea7861050b4d188ed8316505
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-12-19 15:34:48 +1100

    rendering events w user and vice versa

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -18,7 +18,7 @@ class EventsController < ApplicationController
         if !event
             render_record_not_found
         else
-            render json: event, include: :user, except: [:created_at, :updated_at, :id]
+            render json: event.to_json(except: [:created_at, :updated_at, :id, :user_id], include: { user: { only: [:first_name, :last_name] } })
         end
     end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
     def show
         user = User.find_by(id: params[:id])
 
-        render json: user, include: :events # includes events published by user
+        # render json: user, include: :events, except: [:created_at, :updated_at, :id] 
+        render json: user.to_json(except: [:created_at, :updated_at, :id, :password], include: { events: { only: [:title, :date] }})
     end
 end
