8b029e75dfedf8199f550b47f10629cae2b7fedb
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-12-20 22:25:57 +1100

    Linking to React

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { useEffect, useState } from 'react'
+
+function App() {
+  const [events, setEvents] = useState([]);
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
