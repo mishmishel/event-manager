dfca16032c530f3d0817829bbd0633e7046a3c6e
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-12-21 09:42:31 +1100

    Trying to work with React

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 // import { useEffect, useState } from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/Home'; 
 
 function App() {
