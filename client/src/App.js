7f1f8eb659d8644cb2f7154b11965327c28e4a26
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-12-20 22:43:08 +1100

    Linking to React server

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    fetch('/events')
+    .then(response => response.json() )
+    .then(json => {
+      setEvents(json)
+    })
+  }, [])
+
   const [events, setEvents] = useState([]);
   return (
     <div className="App">
-      
+      <h1>Events:</h1>
+      { events.length > 0 
+      ?
+        <ul>
+          {
+            events.map((event)=> {
+              return <li>{event.title}</li>
+            })
+          }
+        </ul>
+      :
+      <p>No Events</p>
+      }
     </div>
   );
 }
