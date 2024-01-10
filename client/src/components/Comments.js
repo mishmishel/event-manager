9072632e7aed65bf625b6ecb49a67d86155859b7
Mishel Tyurkova <misheltyurkova@gmail.com>
2024-01-10 15:58:30 +1100

    Error handling for comments

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate, Link } from 'react-router-dom';
 
 export default function Comments( { user } ) {
   const [comments, setComments] = useState([]);
   const [newComment, setNewComment] = useState('');
   const { id } = useParams(); 
+  const navigate = useNavigate();
+  const [commentError, setCommentError] = useState(false);
 
   useEffect(() => {
     console.log("Event ID:", id);
@@ -19,6 +21,13 @@ export default function Comments( { user } ) {
   const handlePostComment = () => {
     // check if newComment is not empty before making POST request
     if (newComment.trim() !== '') {
+
+      if (!user) {
+        // Redirect to login/signup page for non-authenticated users
+        navigate('/getloggedon');
+        return;
+      }
+
       const user_id = user.id; 
 
       fetch(`/events/${id}/comments`, {
@@ -37,7 +46,14 @@ export default function Comments( { user } ) {
           setComments(prevComments => [...prevComments, newComment]);
           // clear newComment state after posting
           setNewComment('');
+        })
+        .catch(error => {
+          // Handle errors, e.g., display an error message
+          setCommentError('Failed to post the comment. Please try again.');
         });
+    } else {
+      // Display an error message if newComment is empty
+      setCommentError('Please enter a comment before submitting.');
     }
   };
 
@@ -45,12 +61,16 @@ export default function Comments( { user } ) {
     <div>
       <div>
         <label htmlFor="newComment">Post a comment:</label>
+        {commentError && <p>{commentError}</p>}
         <textarea
           id="newComment"
           value={newComment}
           onChange={(e) => setNewComment(e.target.value)}
         />
-        <button onClick={handlePostComment}>Post Comment</button>
+          <button onClick={handlePostComment}>{user ? 'Post Comment' : 'Sign Up'}</button>
+          {!user && (
+            <p>You need to <Link to="/getloggedon">sign up</Link> to post a comment.</p>
+          )}
       </div>
 
       {comments.length > 0 ? (
